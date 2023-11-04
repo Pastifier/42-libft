f12623e47a40abbcbb732aa73f85563d10000fd9
Pastifier <136130975+Pastifier@users.noreply.github.com>
2023-11-04 23:51:28 +0400

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -12,6 +12,18 @@
 
 #include "libft.h"
 
+/*
+	Returns a new string of size `len` that starts from
+ 	`s[start]`.
+
+  	Providing invalid input (out-of-range) should be undefined
+   	behaviour, but I handled those cases for my own future use:
+    	- Providing an index that's larger than the string length
+     	  will result in an empty string.
+	- Providing a size which is larger than the string length
+ 	  will force the size to shrink back to the string's true
+    	  length (provided it's a true C string).
+*/
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*self;
