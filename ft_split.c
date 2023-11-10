5101543b1330b249c2a3c7e2fb7fa93dd9882b83
Pastifier <therealemran.design@gmail.com>
2023-11-10 21:33:12 +0400

    if the 2D-array allocation fails, there's nothing to be freed anyway!

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:45:14 by ebinjama          #+#    #+#             */
-/*   Updated: 2023/11/09 16:52:40 by ebinjama         ###   ########.fr       */
+/*   Updated: 2023/11/10 21:32:22 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,7 @@ char	**ft_split(const char *s, char c)
 		return (NULL);
 	self = malloc((wordcount(s, c) + 1) * sizeof(char *));
 	if (!self)
-	{
-		free(self);
 		return (NULL);
-	}
 	successes = 0;
 	abort = ealloc(self, s, c, &successes);
 	if (abort)
