5074a695cde36226f60edebc43b3336f15e3a422
Emran <therealemran.design@gmail.com>
2024-05-28 08:52:52 +0400

    changed ft_strncmp to stop upon finding a null-terminator in either strings as well

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:00:41 by ebinjama          #+#    #+#             */
-/*   Updated: 2023/11/08 15:29:36 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/05/28 06:11:01 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = -1;
-	while ((size_t)(++i) < n)
+	while (s1[i] && s2[i] && (size_t)(++i) < n)
 	{
 		if (!(s1[i]) || s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
