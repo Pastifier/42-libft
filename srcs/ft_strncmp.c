3b6c2177d0699059360ac22350afa966c591ffdf
Emran <therealemran.design@gmail.com>
2024-06-09 00:54:25 +0400

    lmao

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:00:41 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/05/28 06:11:01 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/06/09 00:50:45 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	i;
 
-	i = -1;
-	while (s1[i] && s2[i] && (size_t)(++i) < n)
+	i = 0;
+	while (s1[i] && s2[i] && (size_t)i < n)
 	{
 		if (!(s1[i]) || s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		++i;
 	}
 	return (0);
 }
